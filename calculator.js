4d97087539aa015b17ac32a46cc19d4353b3e964
mehdi shirazi <msh199271@gmail.com>
2019-10-17 12:05:10 +0330

    Added my front-end side of calculation

---
+++ calculator.js
@@ -0,0 +1,58 @@
+function NumberHandler(e){
+    document.getElementById('result').value += e.currentTarget.value;
+  debugger
+        if(e.currentTarget.value === "+" || e.currentTarget.value === "-"){
+            clickBtn = e.currentTarget.value; 
+            Calculator();
+        }
+        else if(e.currentTarget.value === "="){
+                secondNumber = document.getElementById('result').value;
+                if(clickBtn === "+"){
+                  debugger
+                    alert(`${firstNumber}` + "+" + `${secondNumber}` + "=" + (parseInt(firstNumber)+parseInt(secondNumber)))
+                }
+                else if(clickBtn === "-"){
+                    alert(`${firstNumber}` + "-" + `${secondNumber}` + "=" + (parseInt(firstNumber)-parseInt(secondNumber))) 
+                }
+        }
+}
+
+
+function Calculator(){
+    firstNumber = document.getElementById('result').value; 
+        document.getElementById('result').value = null;
+}
+
+
+let btn1 = document.getElementById('btnNumber1');
+let btn2 = document.getElementById('btnNumber2');
+let btn3 = document.getElementById('btnNumber3');
+let btn4 = document.getElementById('btnNumber4');
+let btn5 = document.getElementById('btnNumber5');
+let btn6 = document.getElementById('btnNumber6');
+let btn7 = document.getElementById('btnNumber7');
+let btn8 = document.getElementById('btnNumber8');
+let btn9 = document.getElementById('btnNumber9');
+let btn0 = document.getElementById('btnNumber0');
+let btnsum = document.getElementById('btnSumption');
+let btnmin = document.getElementById('btnMines');
+let btnequl = document.getElementById('btnEqual');
+let firstNumber = null;
+let secondNumber = null;
+let clickBtn = null;
+
+
+btn1.onclick = NumberHandler;
+btn2.onclick = NumberHandler;
+btn3.onclick = NumberHandler;
+btn4.onclick = NumberHandler;
+btn5.onclick = NumberHandler;
+btn6.onclick = NumberHandler;
+btn7.onclick = NumberHandler;
+btn8.onclick = NumberHandler;
+btn9.onclick = NumberHandler;
+btn0.onclick = NumberHandler;
+btnsum.onclick = NumberHandler;
+btnmin.onclick = NumberHandler;
+btnequl.onclick = NumberHandler;
+
