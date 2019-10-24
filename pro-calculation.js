f6b80e14b9b484d0f26961861c7cf246868b2bbe
mehdi shirazi <msh199271@gmail.com>
2019-10-24 12:22:47 +0330

    Added new file

---
+++ pro-calculation.js
@@ -0,0 +1,54 @@
+function NumberHandler(e){
+    document.getElementById('result').value += e.currentTarget.value;
+    if(e.currentTarget.value === "+" || e.currentTarget.value === "-" || e.currentTarget.value === "*" || e.current.value === "\/"){
+        Multicalculation();
+        oparator.push(e.currentTarget.value);
+        clickBtn = e.currentTarget.value;
+    }
+}
+
+
+function Multicalculation(){
+    result = document.getElementById('result').value
+    if(firstNumber == null){
+        firstNumber = result.substring(0, result.length -1); 
+    }
+    oparator.push(firstNumber);
+    document.getElementById('result').value = null;
+    firstNumber = null;
+}
+
+
+let btn1 = document.getElementById('btnNumber1');
+let btn2 = document.getElementById('btnNumber2');
+let btn3 = document.getElementById('btnNumber3');
+let btn4 = document.getElementById('btnNumber4');
+let btn5 = document.getElementById('btnNumber5');
+let btn6 = document.getElementById('btnNumber6');
+let btn7 = document.getElementById('btnNumber7');
+let btn8 = document.getElementById('btnNumber8');
+let btn9 = document.getElementById('btnNumber9');
+let btn0 = document.getElementById('btnNumber0');
+let btnsum = document.getElementById('btnSumption');
+let btnmin = document.getElementById('btnMines');
+let btnequl = document.getElementById('btnEqual');
+let firstNumber = null;
+let secondNumber = 0;
+let clickBtn = null;
+let oparator = []
+
+
+btn1.onclick = NumberHandler;
+btn2.onclick = NumberHandler;
+btn3.onclick = NumberHandler;
+btn4.onclick = NumberHandler;
+btn5.onclick = NumberHandler;
+btn6.onclick = NumberHandler;
+btn7.onclick = NumberHandler;
+btn8.onclick = NumberHandler;
+btn9.onclick = NumberHandler;
+btn0.onclick = NumberHandler;
+btnsum.onclick = NumberHandler;
+btnmin.onclick = NumberHandler;
+btnequl.onclick = NumberHandler;
+
